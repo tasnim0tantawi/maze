74ecd6b88a72afadaacbe0ea1784917aced0b422
tasnim0tantawi <tasnim0tantawi@gmail.com>
2022-12-21 01:03:48 +0300

    Fixed the ugliness but still ugly

--- maze.py
+++ maze.py
@@ -3,7 +3,12 @@ from math import sqrt
 from search import astar, path_generator
 from colorama import Fore
 
-CAPITAL_LETTERS = [chr(i) for i in range(65, 91)]
+FIXED_NUMBERS = ["0 ", "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "10", "11", "12", "13", "14", "15", "16",
+                 "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33",
+                 "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+                 "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67",
+                 "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84",
+                 "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"]
 
 
 class Cell:
@@ -56,21 +61,24 @@ class Maze:
     def print_maze(self):
         """ Prints the maze. """
         #  Add the top border
-        maze_string = "  "
+        maze_string = "    "
         # adding column numbers
         for i in range(self.columns):
-            maze_string += f"{i} "
+            if int(FIXED_NUMBERS[i]) % 2 == 0:
+                maze_string += Fore.LIGHTCYAN_EX + FIXED_NUMBERS[i] + Fore.RESET
+            else:
+                maze_string += Fore.LIGHTYELLOW_EX + FIXED_NUMBERS[i] + Fore.RESET
         maze_string += "\n"
-        maze_string += Fore.CYAN + "  ╔" + "═" * (self.columns * 2 + 1) + "╗\n" + Fore.RESET
+        maze_string += Fore.CYAN + "  ╔" + "═" * (self.columns * 2 + 2) + "╗\n" + Fore.RESET
         # The maze is a grid of cells
         for row in self.matrix:
-            # adding the row alphabet
-            maze_string += f"{CAPITAL_LETTERS[self.matrix.index(row)]} "
+            # adding the row numbers
+            maze_string += FIXED_NUMBERS[self.matrix.index(row)]
             # Add the left border
             maze_string += Fore.CYAN + "║ " + Fore.RESET
             for cell in row:
                 maze_string += cell + " "
-            maze_string += Fore.CYAN + "║\n" + Fore.RESET
+            maze_string += Fore.CYAN + " ║\n" + Fore.RESET
             # Add the right border
         maze_string += Fore.CYAN + "  ╚" + "═" * (self.columns * 2 + 1) + "╝\n" + Fore.RESET
         # Add the bottom border
